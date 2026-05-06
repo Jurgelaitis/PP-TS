aa832974d635b62928259d20e5624ff373b43bbd
Arūnas Jurgelaitis <arunas.jurgelaitis@gmail.com>
2026-05-06 06:42:36 +0300

    Add files via upload

---
+++ ts_asistentas_modules.js
@@ -0,0 +1,3 @@
+// Šio failo turinys jau įtrauktas (inline) į TS_Asistentas.html.
+// Standalone naudojimui pakanka tik TS_Asistentas.html failo.
+// Šis failas paliktas kaip atsarginė kopija ir gali būti ištrintas.
